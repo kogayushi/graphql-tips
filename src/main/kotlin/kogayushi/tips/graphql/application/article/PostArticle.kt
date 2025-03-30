4888c1dd553ee0f8d12aacfd897b9b1d0c6546e3
koga yushi <youshe.old.pleasure@gmail.com>
2025-03-30 17:59:19 +0900

    post article

---
+++ src/main/kotlin/kogayushi/tips/graphql/application/article/PostArticle.kt
@@ -0,0 +1,28 @@
+package kogayushi.tips.graphql.application.article
+
+import kogayushi.tips.graphql.model.article.Article
+import kogayushi.tips.graphql.model.article.ArticleRepository
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class PostArticle(
+    private val articleRepository: ArticleRepository,
+) {
+    fun handle(inputData: PostArticleInputData): Article {
+        val article = Article(
+            id = UUID.randomUUID(),
+            title = inputData.title,
+            content = inputData.content,
+            authorId = inputData.authorId,
+            likedBy = emptyList()
+        )
+        return articleRepository.add(article)
+    }
+}
+
+data class PostArticleInputData(
+    val title: String,
+    val content: String,
+    val authorId: UUID,
+)
