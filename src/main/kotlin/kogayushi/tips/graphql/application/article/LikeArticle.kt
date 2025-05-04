181b72f406a4dcb88279ce5d1881632d8ed1fbde
koga yushi <youshe.old.pleasure@gmail.com>
2025-05-04 22:52:52 +0900

    publish event

--- src/main/kotlin/kogayushi/tips/graphql/application/article/LikeArticle.kt
+++ src/main/kotlin/kogayushi/tips/graphql/application/article/LikeArticle.kt
@@ -1,7 +1,10 @@
 package kogayushi.tips.graphql.application.article
 
 import kogayushi.tips.graphql.model.article.ArticleRepository
+import kogayushi.tips.graphql.model.article.ArticleUpdatedEvent
+import kogayushi.tips.graphql.model.fundamental.DomainEventPublisher
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
 
 data class LikeArticleInputData(
@@ -11,13 +14,23 @@ data class LikeArticleInputData(
 
 @Service
 class LikeArticle(
-    private val articleRepository: ArticleRepository
+    private val articleRepository: ArticleRepository,
+    private val domainEventPublisher: DomainEventPublisher,
 ) {
+    // DBは使ってないが、TransactionalEventListenerの例を示すためにTransactionalをつけている
+    @Transactional
     fun handle(input: LikeArticleInputData) {
 
         val article = articleRepository.resolveByArticleId(input.articleId)
             ?: throw IllegalArgumentException("Article not found")
         val liked = article.likedBy(input.userId)
         articleRepository.update(liked)
+
+        val event = ArticleUpdatedEvent(
+            id = UUID.randomUUID(),
+            source = liked,
+            occurredAt = java.time.OffsetDateTime.now()
+        )
+        domainEventPublisher.publish(event)
     }
 }
--- src/main/kotlin/kogayushi/tips/graphql/application/article/UnlikeArticle.kt
+++ src/main/kotlin/kogayushi/tips/graphql/application/article/UnlikeArticle.kt
@@ -1,7 +1,10 @@
 package kogayushi.tips.graphql.application.article
 
 import kogayushi.tips.graphql.model.article.ArticleRepository
+import kogayushi.tips.graphql.model.article.ArticleUpdatedEvent
+import kogayushi.tips.graphql.model.fundamental.DomainEventPublisher
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
 
 data class UnlikeArticleInputData(
@@ -11,13 +14,23 @@ data class UnlikeArticleInputData(
 
 @Service
 class UnlikeArticle(
-    private val articleRepository: ArticleRepository
+    private val articleRepository: ArticleRepository,
+    private val domainEventPublisher: DomainEventPublisher,
 ) {
+    // DBは使ってないが、TransactionalEventListenerの例を示すためにTransactionalをつけている
+    @Transactional
     fun handle(input: UnlikeArticleInputData) {
 
         val article = articleRepository.resolveByArticleId(input.articleId)
             ?: throw IllegalArgumentException("Article not found")
         val unliked = article.unlikedBy(input.userId)
         articleRepository.update(unliked)
+
+        val event = ArticleUpdatedEvent(
+            id = UUID.randomUUID(),
+            source = unliked,
+            occurredAt = java.time.OffsetDateTime.now()
+        )
+        domainEventPublisher.publish(event)
     }
 }
